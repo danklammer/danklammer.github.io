70fd522a1ab77b69c4a40838dfe212f10639f0af
Dan Klammer <danklammer@gmail.com>
2016-07-26 03:39:04 -0400

    Cleaned up img-zoom. Slight restructure, giving more flexibility for custom HTML in post pages

--- _includes/img-zoom.js
+++ _includes/img-zoom.js
@@ -1,12 +1,10 @@
 <script>
   [].forEach.call(
-    document.querySelectorAll(".post-content img"), 
+    document.querySelectorAll(".img-zoom"), 
     function(e){
-
-      e.parentElement.classList.add("img-zoom");
-
+        
       e.addEventListener("click", function(zoom){
-        this.parentElement.classList.toggle("zoom");
+        this.classList.toggle("zoom");
       }, false);
 
     }
