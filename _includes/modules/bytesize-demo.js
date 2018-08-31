b3b6e7fb6fbdfa12a615cef6684cfb0f5be1e929
Dan Klammer <danklammer@gmail.com>
2018-08-31 09:20:30 -0400

    Fix toggle bug

--- _includes/modules/bytesize-demo.js
+++ _includes/modules/bytesize-demo.js
@@ -1,21 +1,20 @@
 /* Bytesize Demo */
 
+var icons = document.getElementById("icons");
+
 function changeSize(newVal){
-    var icons = document.getElementById("icons");
     icons.style.fontSize = newVal + 'em';
 }
 
 function changeStroke(newVal){
-    var icons = document.getElementById("icons");
     icons.style.strokeWidth = newVal + '%';
 }
 
 function changeColor(newVal){
-    var icons = document.getElementById("icons");
     icons.style.color = newVal;
 }
 
 function changeStyleRound(){
-    var icons = document.getElementById("icons");
     icons.classList.toggle("round");
+    icons.classList.toggle("square");
 }
