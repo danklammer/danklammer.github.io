a467e7ca96bbe8991549310cdac1f0776f01b217
Dan Klammer <danklammer@gmail.com>
2019-09-06 10:41:02 -0400

    Change loadtime to only show 2 decimals

--- _includes/modules/plugin-loadtime.js
+++ _includes/modules/plugin-loadtime.js
@@ -3,6 +3,6 @@
 window.addEventListener("load", loadTime, false);
 function loadTime() {
   var now = new Date().getTime();
-  var page_load_time = (now - performance.timing.navigationStart) / 1000;
+  var page_load_time = (now - performance.timing.navigationStart) / 100;
   document.getElementById('loadtime').innerHTML = page_load_time;
 }
