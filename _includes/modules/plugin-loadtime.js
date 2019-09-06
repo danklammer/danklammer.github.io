9a2311c773b4d22fd360e81c0d6b31967f133c1a
Dan Klammer <danklammer@gmail.com>
2019-09-06 14:24:03 -0400

    Correct way to keep it to 2 decimal points

--- _includes/modules/plugin-loadtime.js
+++ _includes/modules/plugin-loadtime.js
@@ -4,5 +4,6 @@ window.addEventListener("load", loadTime, false);
 function loadTime() {
   var now = new Date().getTime();
   var page_load_time = (now - performance.timing.navigationStart) / 1000;
+	page_load_time = +page_load_time.toFixed(2);
   document.getElementById('loadtime').innerHTML = page_load_time;
 }
